2d2c2ff37d610b56e679bf4e82316a840c286ce9
Vaclav Snajdr <vaclavsnaj@gmail.com>
2022-05-26 21:48:11 +0200

    initial commit

---
+++ app/MainModule/Presenters/PigLatinPresenter.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MainModule\Presenters;
+
+use Nette;
+
+
+final class PigLatinPresenter extends Nette\Application\UI\Presenter
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function renderDefault(): void
+    {
+        
+    }
+}
