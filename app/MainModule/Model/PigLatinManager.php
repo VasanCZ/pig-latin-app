f4f2edb3012fbe695f3571c61a9164d4c178257f
Vaclav Snajdr <vaclavsnaj@gmail.com>
2022-05-31 17:10:30 +0200

    translate function

--- app/MainModule/Model/PigLatinManager.php
+++ app/MainModule/Model/PigLatinManager.php
@@ -6,6 +6,32 @@ namespace App\MainModule\Model;
 class PigLatinManager {
 
     public function translateToPigLatin(string $inputText): string {
-        return $inputText;
+
+        $outputText = "";
+        
+        $lowerText = strtolower($inputText);
+        $lowerText = trim(preg_replace('/\s+/', ' ', $lowerText)); // Remove more spaces
+        $wordArray = explode(" ", $lowerText);
+        $wordArray = array_filter($wordArray);
+        
+        foreach ($wordArray as $word) {
+            $outputText .= " ";
+            $charArray = str_split($word);          
+            $charIndex = 0;
+
+            if (preg_match('/[aeiouy]/', substr($word, 0, 1))) {
+                $outputText .= $word . "-way";
+                continue;
+            }
+            foreach ($charArray as $char) {
+
+                if (preg_match('/[aeiouy]/', $char)){
+                    $charIndex = strpos($word, $char);
+                    break;
+                }
+            }
+            $outputText .= substr($word, $charIndex) . "-" . substr($word, 0, $charIndex) . "ay";       
+        }
+        return $outputText;
     }
 }
